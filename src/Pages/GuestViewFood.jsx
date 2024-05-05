5de9b2681c57baa8855cb42b93e63c2a00700ed7
Liang Luong <66522372+Luong142@users.noreply.github.com>
2024-05-05 19:00:32 +0800

    Update GuestViewFood.jsx

--- src/Pages/GuestViewFood.jsx
+++ src/Pages/GuestViewFood.jsx
@@ -8,20 +8,29 @@ import './GuestViewFood.css';
 const GuestViewFood = () => {
   const [recipes, setRecipes] = useState([]);
 
-  useEffect(() => {
-    const fetchRecipes = async () => {
-      try {
-        const dietitianId = '3uXPL9DJvFOWYn3jR4W8sW8BHLT2';
+  useEffect(() => 
+  {
+    const fetchRecipes = async () => 
+    {
+      try 
+      {
+        const dietitianId = 'Lj75Thf2h1NKYbZdukN9DKYLExI2';
         const dietitianRef = ref(dbRealtime, `Dietitian Recipe/${dietitianId}`); 
         const dietitianSnapshot = await get(dietitianRef);
 
-        if (dietitianSnapshot.exists()) {
+        if (dietitianSnapshot.exists()) 
+        {
           setRecipes(dietitianSnapshot.val());
-        } else {
+          console.log(dietitianSnapshot.val());
+        } 
+        else 
+        {
           console.log("No recipes found for this dietitian");
           setRecipes([]);
         }
-      } catch (error) {
+      } 
+      catch (error) 
+      {
         console.error("Error fetching recipes:", error);
       }
     };
@@ -33,15 +42,20 @@ const GuestViewFood = () => {
     <div>
       <h2>Recipes-Examples for User</h2>
       <p>This is where users view recipes under view recipes categories.</p>
-      {recipes && Object.keys(recipes).length > 0 ? (
-        Object.keys(recipes).map(recipeId => (
+      {recipes && Object.keys(recipes).length > 0 ? 
+      (
+        Object.keys(recipes).map(recipeId => 
+          (
           <div key={recipeId} className='recipe'>
             <h3>{recipes[recipeId].strMeal}</h3>
             <p>{recipes[recipeId].strInstructions}</p>
             <ul>
-              {recipes[recipeId].ingredients.map((ingredient, index) => (
-                <li key={index}>{ingredient}</li>
-              ))}
+              {recipes[recipeId].ingredientsManual.map((ingredientsManual, index) => 
+              (
+                <li key={index}>{ingredientsManual}</li>
+              )
+              )
+              }
             </ul>
           </div>
         ))
