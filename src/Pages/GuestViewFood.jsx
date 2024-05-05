b6f709dd96470dae469305dde8cb491964fabfb1
FatalGrin <78714201+FatalGrin@users.noreply.github.com>
2024-05-05 19:07:53 +0800

    fixing

--- src/Pages/GuestViewFood.jsx
+++ src/Pages/GuestViewFood.jsx
@@ -1,4 +1,3 @@
-
 import React, { useEffect, useState } from 'react';
 import { dbRealtime } from '../Firebase/Firebase'; 
 import { ref, get } from 'firebase/database'; 
@@ -8,29 +7,20 @@ import './GuestViewFood.css';
 const GuestViewFood = () => {
   const [recipes, setRecipes] = useState([]);
 
-  useEffect(() => 
-  {
-    const fetchRecipes = async () => 
-    {
-      try 
-      {
+  useEffect(() => {
+    const fetchRecipes = async () => {
+      try {
         const dietitianId = 'Lj75Thf2h1NKYbZdukN9DKYLExI2';
         const dietitianRef = ref(dbRealtime, `Dietitian Recipe/${dietitianId}`); 
         const dietitianSnapshot = await get(dietitianRef);
 
-        if (dietitianSnapshot.exists()) 
-        {
+        if (dietitianSnapshot.exists()) {
           setRecipes(dietitianSnapshot.val());
-          console.log(dietitianSnapshot.val());
-        } 
-        else 
-        {
+        } else {
           console.log("No recipes found for this dietitian");
           setRecipes([]);
         }
-      } 
-      catch (error) 
-      {
+      } catch (error) {
         console.error("Error fetching recipes:", error);
       }
     };
@@ -43,25 +33,18 @@ const GuestViewFood = () => {
       <h2>Recipes-Examples for User</h2>
       <p>This is where users view recipes under view recipes categories.</p>
       {recipes && Object.keys(recipes).length > 0 ? 
-      (
-        Object.keys(recipes).map(recipeId => 
-          (
+        Object.keys(recipes).map(recipeId => (
           <div key={recipeId} className='recipe'>
             <h3>{recipes[recipeId].strMeal}</h3>
             <p>{recipes[recipeId].strInstructions}</p>
             <ul>
-              {recipes[recipeId].ingredientsManual.map((ingredientsManual, index) => 
-              (
-                <li key={index}>{ingredientsManual}</li>
-              )
-              )
-              }
+              <li>{recipes[recipeId].meals[0].ingredientsManual.join(', ')}</li>
             </ul>
           </div>
-        ))
-      ) : (
-        <p>No recipes found for this dietitian</p>
-      )}
+        )) : (
+          <p>No recipes found for this dietitian</p>
+        )
+      }
       <div className="back-button">
         <Link to="/guest">Back to Guest Page</Link>
       </div>
