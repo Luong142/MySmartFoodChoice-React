9261045a23b12bd1b43fa41bf8feb5be35c157fe
krithishasmiruthi <krithisha1955@gmail.com>
2024-05-13 23:58:36 +0800

    Reapply "Merge branch 'Prototype' of https://github.com/Luong142/MySmartFoodChoice-React"
    This reverts commit 7b74112b73ae6b96f74b999638c89b1ad02febba.

--- src/HeaderComponents/Header.jsx
+++ src/HeaderComponents/Header.jsx
@@ -10,21 +10,22 @@ function Header() {
     return (
         <nav className='nav'>
             <div className="header-content">
-                <h1 className='title' style={{fontSize: '1.5em'}}>Smart Food Choice</h1>
                 <ul className='nav-links'>
                     <li><Link to="/">
-                        <img src={HomeLogo} alt="Home" width="50" height="20" />
-                        Home
-                    </Link></li>
-                    {!isLoggedIn && (
-                        <>
-                            <li><Link to="/SignIn">Log In</Link></li>
-                            <li><Link to="/SignUp">Sign Up</Link></li>
-                        </>
-                    )}
+                        <img src={HomeLogo}
+                            width="50" 
+                            height="20">
+                            
+                        </img>
+                        Smart Food Choice
+                        </Link>
+                    </li>
+                    <li><Link to="/SignIn">Log In</Link></li>
+                    <li><Link to="/SignUp">Sign Up</Link></li>
                     <li><Link to="/ReviewForm">Leave a Review</Link></li>
                     <li><Link to="/AboutUs">About Us</Link></li>
                     <li><Link to="/ContactUs">Contact Us</Link></li>
+                    
                 </ul>
             </div>
         </nav>
---
+++ src/HeaderComponents/UserHeader.jsx
@@ -0,0 +1,79 @@
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import "./Header.css";
+import { RiUser3Line } from "react-icons/ri";
+import { getDatabase, ref, query, onValue } from "firebase/database";
+import { useNavigate } from "react-router-dom";
+
+function Header() {
+  const [profilePicture, setProfilePicture] = useState("");
+  const userId = localStorage.getItem("uuid");
+  const navigate = useNavigate();
+
+
+  useEffect(() => {
+    const db = getDatabase();
+    const userRef = query(ref(db, `User Profile/${userId}`));
+
+    const unsubscribeUser = onValue(
+      userRef,
+      (snapshot) => {
+        if (snapshot.exists()) {
+          const userData = snapshot.val();
+          console.log(userData);
+          setProfilePicture(userData.profileImageUrl);
+        } else {
+          console.log("User not found");
+        }
+      },
+      (error) => {
+        console.error("Error fetching data: ", error);
+      }
+    );
+
+    return () => unsubscribeUser();
+  }, []);
+
+  const handleLogout  = () => {
+    localStorage.clear();
+    navigate("/")
+  }
+
+  return (
+    <nav className="nav">
+      <div className="header-content">
+        <h1 className="title" style={{ fontSize: "1.5em" }}>
+          Smart Food Choice
+        </h1>
+        <ul className="nav-links">
+          <li>
+            <Link to="/">Log a meal</Link>
+          </li>
+          <li>
+            <Link to="/RoyaltyPoints">Royalty points</Link>
+          </li>
+          <li>
+            <Link to="/ReviewForm">Leave a review</Link>
+          </li>
+          <li>
+            <Link to="/ContactUs">Contact Us</Link>
+          </li>
+          <li>
+            <Link to="/AboutUs">About Us</Link>
+          </li>
+          <li onClick={handleLogout} style={{cursor:"pointer"}}>Logout</li>
+          <li className="avatar-dropdown">
+            <Link to="/CreateUserProfile" className="avatar-icon">
+              {profilePicture ? (
+                <img src={profilePicture} height={50} width={50} />
+              ) : (
+                <RiUser3Line />
+              )}
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+}
+export default Header;
